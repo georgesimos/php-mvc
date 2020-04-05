be03db379da73f02aeb82397211ad30d85184b82
George <admin@georgesimos.com>
2020-04-05 12:58:17 +0300

    add config file

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+    // App Root
+    // echo __FILE__;
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost:8888/simosmvc');
+    // Site Name
+    define('SITENAME', 'SimosMVC');
